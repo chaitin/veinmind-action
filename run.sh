100e5d1a9ad82033155c84ddb738f821b8262004
DVKunion <2622100059@qq.com>
2022-07-06 19:06:08 +0800

    fix: remove run.sh && remove redundant '/data'

--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Install Runner
-docker pull veinmind/veinmind-runner:latest
-docker run --rm --mount 'type=bind,source=/,target=/host,readonly,bind-propagation=rslave' -v /var/run/docker.sock:/var/run/docker.sock -v `pwd`/data:/tool/resource veinmind/veinmind-runner $*
